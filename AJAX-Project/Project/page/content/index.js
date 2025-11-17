96e62f552c1e551d52843cff4b0daeaadd2309f5
kyuumm <kyuuyxx@outlook.com>
2025-11-17 23:54:05 +0800

    Project-14-发布 & 获取文章

--- AJAX-Project/Project/page/content/index.js
+++ AJAX-Project/Project/page/content/index.js
@@ -4,7 +4,52 @@
  *  1.2 获取文章列表数据
  *  1.3 展示到指定的标签结构中
  */
+const queryObj = {
+  status: '',//文章状态，1 待审，2审核通过
+  channel_id: '',//文章频道 id ：空字符串
+  page: 1,//当前页面
+  per_page: 2//当前页面条数
 
+}
+async function setArticleList() {
+  const res = await axios({
+    url: '/v1_0/mp/articles',
+    params: queryObj
+  })
+  console.log(res);
+
+  const htmlStr = res.data.results.map(item => {
+    return `
+    <tr>
+                <td>
+                  <img src="${item.cover.type === 0 ? 'https://img2.baidu.com/it/u=2640406343,1419332367&amp;fm=253&amp;fmt=auto&amp;app=138&amp;f=JPEG?w=708&amp;h=500' : item.cover.images[0]} alt = "" >
+                </td >
+                <td>${item.title}</td>
+                <td>
+                ${item.status === 1 ? `<span class="badge text-bg-success">待审核</span>` : `<span class="badge text-bg-primary">审核通过</span>`}
+                </td>
+                <td>
+                  <span>${item.pubdate}</span>
+                </td>
+                <td>
+                  <span>${item.read_count}</span>
+                </td>
+                <td>
+                  <span>${item.comment_count}</span>
+                </td>
+                <td>
+                  <span>${item.like_count}</span>
+                </td>
+                <td>
+                  <i class="bi bi-pencil-square edit"></i>
+                  <i class="bi bi-trash3 del"></i>
+                </td>
+              </tr > `
+  }).join('')
+
+  document.querySelector('.art-list').innerHTML = htmlStr
+}
+setArticleList();
 /**
  * 目标2：筛选文章列表
  *  2.1 设置频道列表数据
--- AJAX-Project/Project/page/publish/index.js
+++ AJAX-Project/Project/page/publish/index.js
@@ -26,6 +26,37 @@ setChannelList()
  *  2.4 回显并切换 img 标签展示（隐藏 + 号上传标签）
  */
 
+//   <label for="img">封面：</label>
+// < label for= "img" class= "place" > +</label >
+//   <input class="img-file" type="file" name="img" id="img" hidden="">
+//     <img class="rounded"></img>
+//label：点击这个 <label> 时，会触发被关联表单元素的默认行为。因为label有for，和input的id值一样
+//为什么？因为原生input不好修改样式
+document.querySelector('.img-file').addEventListener('change', async e => {
+
+  const file = e.target.files[0]
+
+  if (file) {
+    //这个有点忘了↓
+    const fd = new FormData();
+    fd.append('image', file);
+    const res = await axios({ url: '/v1_0/upload', method: 'POST', data: fd })
+
+    const imgURL = res.data.url;
+    document.querySelector('.rounded').src = imgURL;
+    document.querySelector('.rounded').classList.add('show');
+    document.querySelector('.place').classList.add('hide');
+
+  } else {
+    return
+  }
+
+})
+
+document.querySelector('.rounded').addEventListener('click', e => {
+  //模拟点击用click
+  document.querySelector('.img-file').click()
+})
 /**
  * 目标3：发布文章保存
  *  3.1 基于 form-serialize 插件收集表单数据对象
@@ -33,7 +64,44 @@ setChannelList()
  *  3.3 调用 Alert 警告框反馈结果给用户
  *  3.4 重置表单并跳转到列表页
  */
+document.querySelector('.send').addEventListener('click', async e => {
+  const form = document.querySelector('.art-form')
+  const data = serialize(form, { hash: true, empty: true })
 
+  delete data.id
+  //发布文章不需要id属性，id是为了后续编辑使用的
+  //收集封面
+  data.cover = {
+    type: 1,//封面类型（后端给的）
+    images: [document.querySelector('.rounded').src]
+  }
+
+  try {
+    const res = await axios({
+      url: '/v1_0/mp/articles',
+      method: 'POST',
+      data: data
+    })
+    console.log(res);
+    myAlert(true, '发布成功')
+    form.reset();//只能清空表单元素
+    //封面手动重置
+    document.querySelector('.rounded').src = '';
+    document.querySelector('.rounded').classList.remove('show');
+    document.querySelector('.place').classList.remove('hide');
+    //富文本编辑器重置
+    editor.setHtml();
+
+
+    setTimeout(() => {
+      location.href = '../content/index.html'
+    }, 1500)
+  } catch (error) {
+    console.log(error);
+    myAlert(false, error.response.data.message)
+
+  }
+})
 /**
  * 目标4：编辑-回显文章
  *  4.1 页面跳转传参（URL 查询参数方式）
