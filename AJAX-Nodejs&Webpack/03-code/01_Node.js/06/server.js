34b15478fbd99f7477c947c146f6b6ee6a185138
kyuumm <kyuuyxx@outlook.com>
2025-11-19 23:29:21 +0800

    Nodejs09 CommonJs 模块化

--- AJAX-Nodejs&Webpack/03-code/01_Node.js/06/server.js
+++ AJAX-Nodejs&Webpack/03-code/01_Node.js/06/server.js
@@ -6,24 +6,35 @@
  *  3. 其他路径，暂时返回不存在提示
  *  4. 运行 Web 服务，用浏览器发起请求
  */
-const fs = require('fs')
-const path = require('path')
+
 // 1. 基于 http 模块，创建 Web 服务
-const http = require('http')
-const server = http.createServer()
+const http = require('http');
+const path = require('path')
+
+const server = http.createServer();
+
+const fs = require('fs')
+
 server.on('request', (req, res) => {
   // 2. 使用 req.url 获取请求资源路径，并读取 index.html 里字符串内容返回给请求方
   if (req.url === '/index.html') {
     fs.readFile(path.join(__dirname, 'dist/index.html'), (err, data) => {
-      res.setHeader('Content-Type', 'text/html;charset=utf-8')
-      res.end(data.toString())
+      if (err) console.log(err);
+      else {
+        res.setHeader('Content-Type', 'text/html;charset=utf-8')
+        res.end(data.toString())
+      }
+
     })
-  } else {
-    // 3. 其他路径，暂时返回不存在提示
+  }
+  else {
     res.setHeader('Content-Type', 'text/html;charset=utf-8')
-    res.end('你要访问的资源路径不存在')
+    res.end('404不在')
   }
 })
 server.listen(8080, () => {
-  console.log('Web 服务启动成功了')
+  console.log('启动成功');
+
 })
+
+// 3. 其他路径，暂时返回不存在提示
