835b5e1362e8ee318ee3c84c311250d611fcba94
kyuumm <kyuuyxx@outlook.com>
2025-11-27 11:45:19 +0800

    test

--- AJAX-Nodejs--Webpack/03-code/02_npm/01/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * 目标：基于 CommonJS 标准语法，导入工具属性和方法使用
- */
-
-const obj = require('./utils.js')
-console.log(obj);
-const result = obj.arraySum([1, 2, 3])
-console.log(result);
-//不知道为什么vscode提交不显示记录
--- AJAX-Nodejs--Webpack/03-code/03_Webpack/src/utils/check.js
+++
@@ -1,3 +0,0 @@
-//check.js
-export const checkPhone = (phone) => { return phone.length === 11 }
-export const checkCode = (code) => { return code.length === 6 }
